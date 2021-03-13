1dd78c696796a12aaf2e1d6f8c26169ccd14d3d6
brett <brett.labash@gmail.com>
2021-03-13 13:30:35 -0800

    Add Team component

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,15 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router';
 import useTeam from '../hooks/useTeam';
+import TeamLogo from '../components/TeamLogo';
 
 function Team({ response: teams }) {
   let { entityName } = useParams();
   entityName = entityName.split('-').join(' ');
   const { name } = teams.find((t) => t.name === entityName);
 
-  const { loading, response } = useTeam(name);
-  console.log(response);
-  return <div>{name}</div>;
+  const { loading, response: teamData } = useTeam(name);
+  console.log(teamData);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className='panel'>
+      <TeamLogo id={teamData.id} width='200px' />
+      <h1 className='medium-header'>{teamData.name}</h1>
+      <div style={{ width: '100%' }}>
+        <ul className='info-list row'>
+          <li>
+            Est.<div>{teamData.established}</div>
+          </li>
+          <li>
+            Manager<div>{teamData.manager}</div>
+          </li>
+          <li>
+            Coach<div>{teamData.coach}</div>
+          </li>
+        </ul>
+      </div>
+      <a href={`/${teamData.id}`} className='center btn-main'>
+        {teamData.name} Team Page
+      </a>
+    </div>
+  );
 }
 
 Team.propTypes = {};
